23178b0fc8b6414669deba6699b4aae9384bf7db
meganjam <megan@careerfoundry.com>
2017-08-31 16:40:42 +0200

    text updates

--- src/index.js
+++ src/index.js
@@ -9,19 +9,19 @@ Data: Customize the data below as you please.
 const SKILL_NAME = "Five Minute Recipes";
 const HELP_MESSAGE = "I know how to make tasty meals in less than 5 minutes.";
 const HELP_REPROMPT = "Just ask me for a recipe.";
-const STOP_MESSAGE = "Fine. Eat something else then.";
+const STOP_MESSAGE = "See you next time.";
 const CANCEL_MESSAGE = "Ok.";
 
-const CHOOSE_TYPE_MESSAGE = "Welcome to 5 minute recipes! I know some cool foods. What kind of recipe are you looking for?";
-const REPROMPT_TYPE = "You can choose a breakfast, lunch, snack or dinner recipe.";
+const CHOOSE_TYPE_MESSAGE = "Welcome to five minute recipes! I know some cool breakfast, lunch, snack, or dinner foods. What kind of recipe are you looking for?";
+const REPROMPT_TYPE = "You can choose a breakfast, lunch, snack, or dinner recipe. What type of recipe would you like to choose?";
 const MEALTYPE_NOT_IN_LIST = chosenType => `Sorry, I couldn't find any recipes for ${chosenType}. Do you want a breakfast, lunch, dinner or snack recipe?`;
 
 const SUGGEST_RECIPE = recipeName => `I found this awesome ${recipeName} recipe! Do you want me to tell you how to make ${recipeName}?`;
-const MISUNDERSTOOD_RECIPE_ANSWER = "Please answer with either yes or no.";
+const MISUNDERSTOOD_RECIPE_ANSWER = "Please answer with yes or no.";
 
-const FIRST_TIME_INSTRUCTIONS = "Say 'next' to go to the next line of instructions. Say 'repeat' if you didn't understand me or want to hear the last instructions again.";
-const MISUNDERSTOOD_INSTRUCTIONS_ANSWER = "Sorry I didn't understand you there.";
-const CLOSING_MESSAGE = "Wonderful. Hope you have a great meal or as the Germans say: Guten Appetit!";
+const FIRST_TIME_INSTRUCTIONS = "Say 'next' to go to the next line of instructions. Say 'repeat' if you didn't understand me or want to hear the last line of instructions again.";
+const MISUNDERSTOOD_INSTRUCTIONS_ANSWER = "Sorry, I didn't understand you there.";
+const CLOSING_MESSAGE = "Wonderful. Hope you have a great meal, or as the Germans say, Guten Appetit!";
 
 const recipes = {
   breakfast: [
@@ -31,7 +31,7 @@ const recipes = {
         "You'll need some sandwhich bread, peanut butter and jelly of your choosing.",
         "Find some sandwich bread.",
         "Spread a thick layer of peanut butter onto the bread.",
-        "Dump a huge spoon of jelly on top of the peanut butter and spread it.",
+        "Dump a huge spoonful of jelly on top of the peanut butter and spread it.",
         "There you go! You just made a delicious peanut butter jelly sandwich. Goodbye."
       ]
     },
@@ -71,8 +71,8 @@ const recipes = {
       name: "Sandwich",
       instructions: [
         "Call Subway and order a sandwich you really like.",
-        "Wait until they deliver it. Maybe do some situps. That's really good for your body. So I've heard.",
-        "Once the sandwich deliverer arrives rejoice! You just did something good for your body."
+        "Wait until they deliver it. Maybe do some situps. That's really good for your body, so I've heard.",
+        "Once the sandwich deliverer arrives, rejoice!"
       ]
     },
     {
@@ -90,17 +90,17 @@ const recipes = {
       name: "Frozen Pizza",
       instructions: [
         "Did you know you don't even need to make your own pizza anymore these days? Incredible! Go and buy some!",
-        "Preheat the oven at 180 degrees celcius. If you don't know what that is in Fahrenheit you really should think about your life. Because most countries in the world use celcius. So go ahead and learn it.",
+        "Preheat the oven at 180 degrees celcius.",
         "Once the oven is preheated put the pizza in there. But without the plastic wrap! That's really not fun to eat. I tell you, I knew a guy... but that's a story for another time.",
-        "After 10 to 15 minutes take the pizza out of the oven. If you're really hard core you can wait 20 minutes. I've heard burned pizza is not as unhealthy as people might think."
+        "After 10 to 15 minutes take the pizza out of the oven. If you're really intro crusty pizza, you can wait 20 minutes. I've heard burned pizza is not as unhealthy as people might think."
       ]
     },
     {
       name: "Ice cream",
       instructions: [
-        "Seriously? This was a joke! Who eats ice cream for dinner? But fine. I guess you do. So for this recipe you need ice cream.",
-        "Open your freezer, get the ice cream and a small spoon. Only weird people use big spoons to eat ice cream. Don't be weird.",
-        "Enjoy the ice cream. Don't take too big bites! That would potentially be really bad."
+        "For this recipe you just need ice cream.",
+        "Open your freezer, get the ice cream and a big spoon. Only weird people use small spoons to eat ice cream. Don't be weird.",
+        "Enjoy the ice cream. Don't take too big of a bite! You might get brain freeze."
       ]
     },
     {
@@ -111,7 +111,7 @@ const recipes = {
         "Use a black pan and start heating it up.",
         "Put the steak in the heated pan and shortly sear it from both sides.",
         "Now, turn down the heat on the stove and keep frying the steak for as long as you want. It really depends on how medium or well done you like your steak. So it's kind of hard for me to tell you how long to fry it. Just do what your heart tells you to do.",
-        "Perfect. This is some really fancy meal."
+        "Perfect. This is a really fancy meal."
       ]
     }
   ],
@@ -128,16 +128,16 @@ const recipes = {
       name: "Banana",
       instructions: [
         "If you happen to live in the jungle, you might find a banana on a tree somewhere. If you don't, you might have to go to a market close by.",
-        "Pick a yellow banana. Don't pick a green banana. Those have to sit for a while before being eatable. And we want this recipe to be done within 5 minutes. So seriously, don't screw this up. Pick a yellow banana!",
+        "Pick a yellow banana. Don't pick a green banana. Those have to sit for a while before being edible. And we want this recipe to be done within 5 minutes. So seriously, don't screw this up. Pick a yellow banana!",
         "Feeling like a monkey today? Well that's important sometimes, too."
       ]
     },
     {
       name: "Beef Jerkey",
       instructions: [
-        "Now, this is a tricky one. Are you sure you want to eat beef jerkey? I'm not sure if that's really a good idea. But fine. Whatever you want. Just go to a grocery store and buy some.",
-        "Great! Now hold your breath! You don't want to breathe in while opening a bag of beef jerkey. That's really not nice. So quick, wrip open the bag. Wait for 10 seconds and then walk into a different room with the open bag.",
-        "Now, you may breathe again. But don't go crazy. The smell is still there. It's just not as strong. Have fun eating."
+        "Go to a grocery store and buy some beef jerkey.",
+        "Great! Now open the bag.",
+        "Have fun eating."
       ]
     }
   ]
